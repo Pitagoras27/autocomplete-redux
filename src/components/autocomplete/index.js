ec34b61104ba4abafe5bf399591f5e83722d1a69
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-06-16 11:35:53 -0500

    Se agrega propTypes dependencie

--- src/components/autocomplete/index.js
+++ src/components/autocomplete/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import InputBase from '@material-ui/core/InputBase';
+import Paper from '@material-ui/core/Paper'
+import MenuItem from '@material-ui/core/MenuItem';
 import SearchIcon from '@material-ui/icons/Search';
 import './styles.css';
 
 class Autocomplete extends Component {
+  state={
+    isOpen:false,
+  }
   render() {
     const {
       onChangeText,
-      text } = this.props;
+      text,
+      suggestions,
+      onChangeSelection,
+     } = this.props;
+    const { isOpen } = this.state;
+
     return (
       <div className="main-container">
         <div className="container-icon">
@@ -23,9 +34,36 @@ class Autocomplete extends Component {
           onBlur={() => {}}
           onFocus={() => {}}
         />
+        {isOpen &&
+          <Paper className="container-results" square>
+              {suggestions.map(suggestion =>
+              <MenuItem
+                  key={suggestion.id}
+                  component="div"
+                  onClick={() => {
+                      onChangeSelection(suggestion.title);
+                      this.setState({ isOpen: false });
+                  }}
+              >
+                  {suggestion.title}
+              </MenuItem>)}
+          </Paper>}
       </div>
     );
   }
 }
 
+Autocomplete.propTypes= {
+  text: PropTypes.string,
+  suggestions: PropTypes.array,
+  onChangeSelection: PropTypes.func,
+  onChangeText: PropTypes.func,
+}
+
+Autocomplete.defaultProps= {
+  text: '',
+  suggestions: [],
+  onChangeSelection: () => {},
+  onChangeText: () => {},
+}
 export default Autocomplete;
