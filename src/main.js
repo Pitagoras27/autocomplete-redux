151346f935ffd674e8e1a72e584a47f45acb8f08
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2021-01-02 20:13:16 -0600

    tercer commit en repositorio remoto. prueba con rebase -i

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ class Main extends Component {
 
   onSetCurrentPage = (page, idItem) => {
     console.log('Segundo commit en el repositorio remoto, prueba con rebase i')
+    console.log('Tercer commit en repositiorio remoto, prueba con rebase i');
     this.setState({
       onPage: page,
       idItem,
